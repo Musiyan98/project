537784186eb12b7682124e0fa787a25aa889bd0a
Musiyan <musiyan98@gmail.com>
2022-02-04 17:17:22 +0200

    MiratexHousePrevFinal.

--- newProject/MiratexHouse/JS/script.js
+++ newProject/MiratexHouse/JS/script.js
@@ -24,17 +24,47 @@ document.querySelector('.btn-prev').addEventListener('click', function() {
 function showBenefits1 (el) {
     if(document.querySelector('.benefitsScroolDiv1').clientHeight === 90) {
         document.querySelector('.benefitsScroolDiv1').style.height = "100%";
-    } else {
-        document.querySelector('.benefitsScroolDiv1').style.height = "90px";
-        
-    }
+        document.querySelector('#benefits-buttom1').style.opacity = 0;
+        document.querySelector('#benefits-buttom1').style.visibility = "hidden"
+        document.querySelector('#benefits-buttom1-2').style.opacity = 1;
+        document.querySelector('#benefits-buttom1-2').style.visibility = "visible"
+
+    }    else {
+            document.querySelector('.benefitsScroolDiv1').style.height = "90px";
+            document.querySelector('#benefits-buttom1-2').style.opacity = 0;
+            document.querySelector('#benefits-buttom1-2').style.visibility = "hidden"
+            document.querySelector('#benefits-buttom1').style.opacity = 1;
+            document.querySelector('#benefits-buttom1').style.visibility = "visible"
+        }
 }
 
 function showBenefits2 (el) {
     if(document.querySelector('.benefitsScroolDiv2').clientHeight === 90) {
         document.querySelector('.benefitsScroolDiv2').style.height = "100%";
-    } else {
-        document.querySelector('.benefitsScroolDiv2').style.height = "90px";
-        
-    }
+        document.querySelector('#benefits-buttom2').style.opacity = 0;
+        document.querySelector('#benefits-buttom2').style.visibility = "hidden"
+        document.querySelector('#benefits-buttom2-2').style.opacity = 1;
+        document.querySelector('#benefits-buttom2-2').style.visibility = "visible"
+    }   else {
+            document.querySelector('.benefitsScroolDiv2').style.height = "90px";
+            document.querySelector('#benefits-buttom2-2').style.opacity = 0;
+            document.querySelector('#benefits-buttom2-2').style.visibility = "hidden"
+            document.querySelector('#benefits-buttom2').style.opacity = 1;
+            document.querySelector('#benefits-buttom2').style.visibility = "visible"
+        }
 }
+
+
+
+
+// function showcontact() {
+//     if (document.querySelector('.popup').style.opacity = '0';) { 
+//         document.querySelector('.popup').style.visibility = 'visible';
+//         document.querySelector('.popup').style.opacity = '1';
+//     }   else { 
+//         document.querySelector('.popup').style.visibility = 'hidden';
+//         document.querySelector('.popup').style.opacity = '0';
+        
+//     }
+
+// }
